0b2ace89b0a846415d3e8f4d1ea6097072c0c225
Dag-Roger Eriksen <dag.roger.e@gmail.com>
2016-02-13 19:56:06 +0100

    prøvde litt forskjelig

--- lab5/task5/index.php
+++ lab5/task5/index.php
@@ -2,9 +2,15 @@
 $data = file_get_contents("data.txt");
 
 $ret = array_map (
-  function ($_) {return explode (',', $_);},
+  function ($_) {return explode (', ', $_);},
+
   explode ('\n', $data)
+
 );
+
+// $ret["name"] = $ret["1"];
+// unset($ret["1"]);
+
 echo '<pre>';
 print_r ($ret);
 echo '</pre>';
